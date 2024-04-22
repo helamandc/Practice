8729fcff69d5f558e0c9d54fdfa02658534b7a4d
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2024-04-23 07:32:34 +1000

    Practice using system random

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,37 @@
+﻿// Random dice = new Random();
+// int roll = dice.Next(1,6);
+// Console.WriteLine(roll);
+
+// Random dice = new Random();
+// int Random.Next();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50,101);
+// int roll4 = dice.Next(-5,-1);
+
+// Console.WriteLine($"Roll1: {roll1}");
+// Console.WriteLine($"Roll2: {roll2}");
+// Console.WriteLine($"Roll3: {roll3}");
+// Console.WriteLine($"Roll4: {roll4}");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue,secondValue);
+
+
+Console.WriteLine(largerValue);
+
+
+// if(firstValue < secondValue)
+// {
+//     largerValue = secondValue;
+//     Console.WriteLine($"The larger value is {largerValue}.");
+// }
+// else
+// {
+//     largerValue = firstValue;
+//     Console.WriteLine($"The larger value is {largerValue}.");
+// }
+
