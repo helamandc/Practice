ebe1ad46f11c4c614e8ce43bb2ee6de344424a88
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2024-04-24 07:35:27 +1000

    Boolean expressions

--- DecisionPractice/CsharpProjects/TestProject/Program.cs
+++ DecisionPractice/CsharpProjects/TestProject/Program.cs
@@ -1,33 +1,46 @@
-﻿// using System.Net;
+﻿using System.Net;
 
-// Random dice = new Random();
+Random dice = new Random();
 
-// int roll1 = dice.Next(1,7);
-// int roll2 = dice.Next(1,7);
-// int roll3 = dice.Next(1,7);
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
 
-// int total = roll1 + roll2 + roll3;
+int total = roll1 + roll2 + roll3;
 
-// if (total  > 14)
-// {
-// Console.WriteLine("You win!");
-// }
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-// if(total < 15)
-// {
-// Console.WriteLine("Sorry, you lose.");
-// }
-
-// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-string message = "the quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if(message.Contains("fox"))
+if(roll1 == roll2 || roll2 == roll3 || roll3 == roll1)
 {
-Console.WriteLine("What does the foz say?");
+Console.WriteLine("You rolled doubles! +2 bonus to total!");
+total += 2;
 }
+
+if(roll1 == roll2 && roll2 == roll3)
+{
+Console.WriteLine("You rolled triples! +6 bonus to total!");
+}
+
+if (total  >= 15)
+{
+Console.WriteLine("You win!");
+}
+
+if(total < 15)
+{
+Console.WriteLine("Sorry, you lose.");
+}
+
+
+
+// string message = "the quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if(message.Contains("fox"))
+// {
+// Console.WriteLine("What does the foz say?");
+// }
 
 
 
